da612f5bb89b4328f56301ef636deea4f998334f
marlepo <marcinlpotrykus@gmail.com>
2019-12-10 19:49:17 +0100

    update general

--- assets/getDataJSON.js
+++ assets/getDataJSON.js
@@ -1,23 +1,11 @@
-const https = require("https");
+const fetch = require("node-fetch");
 const convert = require("xml-js");
 
-let getDataJSON = _url => {
-  return new Promise((resolve, reject) => {
-    var data = "";
-
-    https.get(_url, response => {
-      if (response.statusCode >= 200 && response.statusCode < 400) {
-        response.on("data", data_ => {
-          data += data_.toString();
-        });
-        response.on("end", () => {
-          let convertedArmag = JSON.parse(convert.xml2json(data, { compact: true, spaces: 2 }));
-          
-          resolve(convertedArmag);
-        });
-      }
-    });
-  });
+let getDataJSON = async _url => {
+  const response = await fetch(url);
+  const text = await response.text();
+  const data = convert.xml2js(text, { compact: true });
+  return data;
 };
 
 module.exports = getDataJSON;
