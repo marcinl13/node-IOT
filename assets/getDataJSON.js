1e6d4212f61acbc66a76628a6d1e7d3d1e0f099c
marlepo <marcinlpotrykus@gmail.com>
2019-12-10 19:54:06 +0100

    Update getDataJSON.js

--- assets/getDataJSON.js
+++ assets/getDataJSON.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const convert = require("xml-js");
 
 let getDataJSON = async _url => {
-  const response = await fetch(url);
+  const response = await fetch(_url);
   const text = await response.text();
   const data = convert.xml2js(text, { compact: true });
   return data;
