c6cef4a6b3d5c745b73c40f18b6bdd570e97ccb0
marlepo <marcinlpotrykus@gmail.com>
2019-12-17 20:43:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,12 @@ const checkStatus = require("./assets/checkStatus");
 
 const PORT = 5000;
 
-app.get("/:latitude/:longitude", async (req, res, next) => {
+app.get("/:latitude/:longitude/:debug?", async (req, res, next) => {
   const latitude = req.params.latitude ? parseFloat(req.params.latitude) : null;
   const longitude = req.params.longitude ? parseFloat(req.params.longitude) : null;
+  const isDebug = false;
 
-  const preparedData = await prepareData(latitude, longitude, isDebug);
-  
-  res.send(preparedData);
-});
-
-app.get("/:latitude/:longitude/:debug", async (req, res, next) => {
-  const latitude = req.params.latitude ? parseFloat(req.params.latitude) : null;
-  const longitude = req.params.longitude ? parseFloat(req.params.longitude) : null;
-  const isDebug = req.params.debug ? (req.params.debug.toLocaleLowerCase() === "debug" ? true : false) : false;
+  if (req.params.debug !== undefined) isDebug = req.params.debug == "debug" ? true : false;
 
   const preparedData = await prepareData(latitude, longitude, isDebug);
 
