b4a55e8915943edc4c69af587e3cedbd3ac8f6f4
marlepo <marcinlpotrykus@gmail.com>
2019-12-17 20:39:36 +0100

    catch

--- assets/checkStatus.js
+++ assets/checkStatus.js
@@ -6,16 +6,20 @@ let checkStatus = async (req, res, next) => {
   await Promise.all([
     parseRemoteXML("https://armaag.gda.pl/data/xml/weather.xml"), // temperature & humidity
     parseRemoteXML("https://armaag.gda.pl/data/xml/stacje_porownawcze.xml") // pm10
-  ]).then(response => {
-    if (
-      response[0].document.station.length === acceptableAmountOfStations &&
-      response[1].document.station.length === acceptableAmountOfStations
-    ) {
-      res.send({ status: "ok" });
-    } else {
-      res.send({ status: "error", message: "dane niekompletne" });
-    }
-  });
+  ])
+    .then(response => {
+      if (
+        response[0].document.station.length === acceptableAmountOfStations &&
+        response[1].document.station.length === acceptableAmountOfStations
+      ) {
+        res.send({ status: "ok" });
+      } else {
+        res.send({ status: "error", message: "dane niekompletne" });
+      }
+    })
+    .catch(e => {
+      res.send({ status: "error", message: e });
+    });
 };
 
 module.exports = checkStatus;
